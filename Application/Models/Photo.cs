911def81e57723691427fdadfe0531d1cd445c9f
Ergyun Tagiew <ergyun.tagiew@gmail.com>
2022-04-29 08:39:18 +0400

    Added main models.

---
+++ Application/Models/Photo.cs
@@ -0,0 +1,10 @@
+﻿using Base;
+
+namespace Application.Models;
+
+public class Photo: IModel
+{
+    public int Id { get; set; }   
+    public string Path { get; set; }
+    public string Alt { get; set; }
+}
---
+++ Application/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace Application.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string AdaId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public Photo Photo { get; set; }
+}
